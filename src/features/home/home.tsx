35ca5231a29d67f52dcd56202a037e7a451cfce2
Amos Kohen <benjaminmasor@gmail.com>
2023-04-01 15:58:59 -0700

    add: home page

---
+++ src/features/home/home.tsx
@@ -0,0 +1,18 @@
+import SearchForm from '../../components/search-form/search-form';
+import Logo from '../../logo.svg';
+
+import './home.scss';
+
+function Home() {
+  return (
+    <section className="home">
+      <img src={Logo} className="app-logo" alt="logo" />
+
+      <h1>Welcome to GitHub Gist Search!</h1>
+
+      <SearchForm />
+    </section>
+  );
+}
+
+export default Home;
